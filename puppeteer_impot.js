dd080de4602bafbff045562d7bd585990f8c683d
stephane bard <stephane.bard@gmail.com>
2019-05-08 14:35:29 +0200

    evolve tax script

--- puppeteer_impot.js
+++ puppeteer_impot.js
@@ -41,51 +41,17 @@ process.argv.forEach(function (val, index, array) {
   if(/--full/.test(val)){ restricted = false; }
 });
 
-
-const search_available_document_type = async (page, type_impot) => {
-  await page.waitForSelector('form[name="documentsForm"]');
-  await page.waitForSelector('form[name="documentsForm"] select[name="typeImpot"]');
-  await page.evaluate((type_impot)=>{document.querySelector('form[name="documentsForm"] select[name="typeImpot"]').value=type_impot}, type_impot);
-  await page.waitFor(200);
-  await page.evaluate(()=>{redirect(document.forms[0].typeImpot.options.selectedIndex);});
-  await page.waitFor(2000);
-
-  var lst_documents = await page.evaluate((type_impot)=>{
-    var lst_documents = [];
-    document.querySelectorAll('form[name="documentsForm"] select[name="annee"] option').forEach(function(item){
-       if(/[0-9]{4}/.test(item.value)){
-         lst_documents.push({'type_impot':type_impot, 'annee':item.value, 'type_document':'avis'});
-       }
-    });
-    return lst_documents;
-  }, type_impot);
-  await page.waitFor(200);
-  return lst_documents;
-}
-
-
-const select_document_type = async (page, type_impot, type_document, annee) => {
-  await page.waitForSelector('form[name="documentsForm"]');
-  await page.waitForSelector('form[name="documentsForm"] select[name="typeImpot"]');
-  await page.waitForSelector('form[name="documentsForm"] select[name="typeDocument"]');
-  await page.waitForSelector('form[name="documentsForm"] select[name="annee"]');
-  await page.waitForSelector('a.cssBouton');
-
-  await page.evaluate((type_impot)=>{document.querySelector('form[name="documentsForm"] select[name="typeImpot"]').value=type_impot}, type_impot);
-  await page.waitFor(200);
-  await page.evaluate((annee)=>{document.querySelector('form[name="documentsForm"] select[name="annee"]').value=annee}, annee);
-  await page.waitFor(200);
-  await page.evaluate((type_document)=>{document.querySelector('form[name="documentsForm"] select[name="typeDocument"]').value=type_document}, type_document);
-  await page.waitFor(200);
-  await page.evaluate(()=>{document.querySelector('a.cssBouton').click()});
-  await page.waitFor(200);
-}
-
-const read_documents = async (browser, page) => {
+const read_documents = async (page) => {
 
     // let's store current documents
-    let lst_documents = [];
-    var _lst_documents = await page.evaluate(function(){
+    let lst_documents = await page.evaluate(function(){
+
+      const serialize_form = (myForm)=>{
+        let formEntries = new FormData(myForm).entries();
+        let _obj = Object.assign(...Array.from(formEntries, ([x,y]) => ({[x]:y})));
+        return _obj;
+      };
+
       String.prototype.sansAccent = function(){
           var accent = [
                   /[\300-\306]/g, /[\340-\346]/g, // A, a
@@ -105,117 +71,68 @@ const read_documents = async (browser, page) => {
            
           return str;
       }
-      var link;
-      var url='';
-      var annee='';
-      var full_text_link='';
-      var text_link ='';
       var result_list = [];
-      var lst_links = document.querySelectorAll('a.cssLienTable');
-      var type = '';
+      var annee = document.querySelector('div.blocAnnee.selected').innerText;
+      let previous_year = (parseInt(annee, 10) -1).toString();
+      var lst_texts = document.querySelectorAll('div.documents div.row div.texte');
+      var lst_forms = document.querySelectorAll('div.documents div.row form');
 
-      for(var i=0;i<lst_links.length;i++){
-        link = lst_links[i];
-        if(i===0){
-          annee     = link.parentElement.previousElementSibling.previousElementSibling.innerText;
-          text_link = link.parentElement.previousElementSibling.innerText;
-        }
-        full_text_link = 'annee_' + annee + '_' + text_link + '_' + link.innerText;
-        full_text_link = full_text_link.sansAccent().toLowerCase();
-        full_text_link = full_text_link.replace(/ /g, '_');
-        full_text_link = full_text_link.replace(/\s/g,'_');
-        full_text_link = full_text_link.replace(/-/g,'_');
-        full_text_link = full_text_link.replace(/[^a-z0-9A-Z]/g,'_');
-        full_text_link = full_text_link.replace(/'/g,'');
-        full_text_link = full_text_link.replace(/_{2,}/g,'_')+'.pdf';
+      for(var i=0;i<lst_texts.length;i++){
 
-        url = link.getAttribute('onclick');
-        url = url.split("'")[1].split("'")[0];
+        let text_link     = lst_texts[i].innerText;
+        let document_form = lst_forms[i];
+        let serialized_form = serialize_form(document_form);
+
+        let full_text_link     = 'annee_' + annee + '_' + text_link;
+        full_text_link         = full_text_link.sansAccent().toLowerCase();
+        full_text_link         = full_text_link.replace(/ /g, '_');
+        full_text_link         = full_text_link.replace(/\s/g,'_');
+        full_text_link         = full_text_link.replace(/-/g,'_');
+        full_text_link         = full_text_link.replace(/[^a-z0-9A-Z]/g,'_');
+        full_text_link         = full_text_link.replace(/'/g,'');
+        full_text_link         = full_text_link.replace(/_{2,}/g,'_')+'.pdf';
+        full_text_link         = full_text_link.replace(/_.pdf$/g,'.pdf');
+        let org_full_text_link = full_text_link;
+       
 
         type = '';
         if(/habitation/.test(full_text_link)){
           type='taxe_habitation';
         } else if(/foncieres/.test(full_text_link)){
           type='taxe_foncieres';
+
         } else if(/revenus/.test(full_text_link)){
           type='impot_sur_les_revenus';
         }
-
         result_list.push({ 'annee'         : annee,
                            'name'          : full_text_link,
-                           'url'           : url,
-                           'type_document' : type });
+                           'org_name'      : org_full_text_link,
+                           'form'          : serialized_form,
+                           'type_document' : type,
+                           'detail_form'   : {
+                                             'action':document_form.getAttribute('action'),
+                                             'method':document_form.getAttribute('method')
+                                            } });
       }
       return result_list;
     });
-    lst_documents = [];
-    Array.prototype.push.apply(lst_documents, _lst_documents);
 
-    let doc;
-    let targetPromise;
-    let target;
-    let new_page;
-    let error_happend;
+    await page.waitFor(200);
 
+    for(var _i=0;_i<lst_documents.length;_i++){
+      let _doc = lst_documents[_i];
 
-    for(i=0;i<lst_documents.length;i++){
-      doc = lst_documents[i];
-
-      targetPromise = new Promise((resolve, reject) => {
-        const listener = async (target) => {
-          try {
-            if (target.type() === 'page' && /ConsultationDocument/.test(target.url())) {
-              browser.removeListener('targetcreated', listener);
-              resolve(target);
-            }
-          } catch (error) {
-            reject(error);
-          }
-        };
-        browser.addListener('targetcreated', listener);
-      });
-
-      await page.evaluate(function(url){
-        win = ouvreDocument(url, 805, 500);
-      }, doc.url);
-
-      target = await targetPromise
-          .then((target)=>{return target;})
-          .catch(function(error){if(error){console.log(error);}});
-
-      await page.waitFor(5000);
-
-      try {
-        new_page = await target.page();
-        await new_page.waitFor(100);
-        error_happend = await new_page.evaluate(()=>{return document.querySelector('img[src$="attention.gif"]')!==null})
-        if(!error_happend){
-          await new_page.waitFor('iframe');
-          await new_page.waitFor(100);
-
-          var __url = await new_page.evaluate(function(){
-            return document.querySelector('iframe').src;
-          });
-
-          if(! await fs.existsSync(aim_path+doc.annee)){
-            await fs.mkdirSync(aim_path+doc.annee);
-          }
-          if(! await fs.existsSync(aim_path+doc.annee+'/'+doc.type_document)){
-            await fs.mkdirSync(aim_path+doc.annee+'/'+doc.type_document+'/');
-          }
-
-          if(! await fs.existsSync(aim_path+doc.annee+'/'+doc.type_document+'/'+doc.name)){
-            await page.evaluate(utils.download_it, __url, aim_path+doc.annee+'/'+doc.type_document+'/'+doc.name)
-                      .then(utils.save_download)
-                      .catch(function(error){if(error){console.log(error);}});
-          }
-        }
-        await new_page.close();
-      } catch (e) {
-         console.log('Error: ' + e);
+      if(! await fs.existsSync(aim_path+_doc.annee)){
+        await fs.mkdirSync(aim_path+_doc.annee);
+      }
+      if(! await fs.existsSync(aim_path+_doc.annee+'/'+_doc.type_document)){
+        await fs.mkdirSync(aim_path+_doc.annee+'/'+_doc.type_document+'/');
       }
 
-      await page.waitFor(200);
+      await page.evaluate(utils.download_it, 'https://cfspart.impots.gouv.fr/enp/ensu/document.do?idEnsua='+_doc.form.idEnsua, aim_path+_doc.annee+'/'+_doc.type_document+'/'+_doc.name)
+                .then(utils.save_download)
+                .catch(function(error){if(error){console.log(error);}});
+
     }
 }
 
@@ -239,39 +156,42 @@ if(aim_path!==null && identifiant !== null && password !== null){
 
       await page.waitForSelector('a.btn.identificationpart');
       await page.click('a.btn.identificationpart');
-      await page.waitForSelector('input[name="LMDP_Spi_tmp"]');
-      await page.waitForSelector('input[name="LMDP_Password_tmp"]');
+      await page.waitForSelector('#btnAction');
+      await page.waitFor(1000);
+      await page.waitForSelector('#spi_tmp');
 
+      await page.waitFor(1000);
+      await page.type('#spi_tmp', identifiant);
       await page.waitFor(500);
-      await page.type('#LMDP_formulaire input[name="LMDP_Spi_tmp"]', identifiant);
+      await page.click('#btnAction');
+
+      await page.waitForSelector('#pwd_tmp', {visible:true});
+      await page.type('#pwd_tmp', password);
       await page.waitFor(500);
-      await page.type('#LMDP_formulaire input[name="LMDP_Password_tmp"]', password);
+      await page.click('#btnAction');
       await page.waitFor(1000);
 
-      await page.click('#LMDP_formulaire button[type="submit"]');
-      await page.waitForSelector('#personalInfo p');
-      await page.goto('https://cfspart.impots.gouv.fr/acces-usager/cfs');
-      await page.waitForSelector('a[title="Acc\u00e9der aux avis et d\u00e9clarations"]');
-      await page.click('a[title="Acc\u00e9der aux avis et d\u00e9clarations"]');
-      await page.waitForSelector('form[name="documentsForm"]');
+      await page.waitForSelector('#accederdeconnexion');
+      await page.click('a[href="documents.do?n=0"]');
 
+      await page.waitForSelector('div.container.general');
 
       let lst_annees = await page.evaluate((restricted) => {
-          var _item, i, lst_options, max_year;
+          var _item, i, lst_items, max_year, result;
           result = [];
-          lst_options = document.querySelectorAll('select[name="annee"] option');
+          lst_items = document.querySelectorAll('div.row.date a[href^="documents.do?n="]');
           max_year = 0;
-          for(i=0;i<lst_options.length;i++){
-            _item = lst_options[i];
+          for(i=0;i<lst_items.length;i++){
+            _item = lst_items[i];
             if(restricted){
-              __item = parseInt(_item.value, 10);
+              __item = parseInt(_item.innerText, 10);
               if(max_year<__item){
                 max_year = __item;
               }
 
             } else {
-              if(/[0-9]{4}/.test(_item.value)){
-                result.push(_item.value);
+              if(/[0-9]{4}/.test(_item.innerText)){
+                result.push(_item.innerText);
               }
             }
           }
@@ -281,35 +201,23 @@ if(aim_path!==null && identifiant !== null && password !== null){
           return result;
         }, restricted);
 
-      let lst_entries_IR = await search_available_document_type(page, 'IR');
-      let lst_entries_TH = await search_available_document_type(page, 'TH');
-      let lst_entries_TF = await search_available_document_type(page, 'TF');
-      let lst_entries = [];
-      let item;
 
-      Array.prototype.push.apply(lst_entries, lst_entries_IR);
-      Array.prototype.push.apply(lst_entries, lst_entries_TF);
-      Array.prototype.push.apply(lst_entries, lst_entries_TH);
 
-      await page.goto('https://cfspart.impots.gouv.fr/acces-usager/cfs');
-      await page.waitForSelector('a[title="Acc\u00e9der aux avis et d\u00e9clarations"]');
-      await page.waitFor(1000);
-      await page.click('a[title="Acc\u00e9der aux avis et d\u00e9clarations"]');
-      await page.waitFor(2000);
-
-      for(var _i=0;_i<lst_entries.length;_i++){
-        item = lst_entries[_i];
+      for(var _i=0;_i<lst_annees.length;_i++){
+        annee = lst_annees[_i];
         // check folder is available
-        if(! await fs.existsSync(aim_path+item.annee)){
-          await fs.mkdirSync(aim_path+item.annee);
+        if(! await fs.existsSync(aim_path+annee)){
+          await fs.mkdirSync(aim_path+annee);
         }
-
-        // impot revenu
-        select_document_type(page, item.type_impot, item.type_document, item.annee);
+        await page.click('a[href="documents.do?n='+annee+'"]');
         await page.waitFor(1000);
-        await read_documents(browser, page);
-        await page.waitFor(2000);
+        await read_documents(page);
       }
+      
+      await page.click('#accederdeconnexion');
+      await page.waitForSelector('#confirmdeconnexion',{'visible':true});
+      await page.click('#confirmdeconnexion');
+      await page.waitFor(1000);
 
     } catch (error) {
       console.log(error);
